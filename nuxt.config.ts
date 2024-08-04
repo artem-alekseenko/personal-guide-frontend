6d0d5abfd74ab922c6ea32ab7bbfb124796b1bbc
artem-alekseenko <ivanovo.cwi@gmail.com>
2024-08-04 16:09:33 +0300

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@vueuse/nuxt"],
+  modules: [
+      "@vueuse/nuxt",
+      "@nuxtjs/tailwindcss",
+  ],
 
   future: {
     compatibilityVersion: 4,
