b398df7f768bf8ccae7d8dead0105fc69473b79e
artem-alekseenko <ivanovo.cwi@gmail.com>
2024-11-18 23:58:34 +0300

    add login

---
+++ components/PGAuthForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container flex max-w-md flex-col justify-center gap-4 p-10">
+    <UInput
+      v-model="email"
+      class="block"
+      placeholder="Email"
+      type="email"
+      variant="outline"
+    />
+    <UInput
+      v-model="password"
+      class="block"
+      placeholder="Password"
+      type="password"
+      variant="outline"
+    />
+    <UButton
+      :loading="state === STATE.REQUESTING_USER"
+      block
+      variant="outline"
+      @click="login"
+      >Login</UButton
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+enum STATE {
+  INITIAL = "INITIAL",
+  REQUESTING_USER = "REQUESTING_USER",
+  USER_FOUND = "USER_FOUND",
+  ERROR_REQUEST = "ERROR_REQUEST",
+}
+
+const state = ref(STATE.INITIAL);
+const email = ref("");
+const password = ref("");
+
+const auth = getAuth();
+
+const login = async () => {
+  state.value = STATE.REQUESTING_USER;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value,
+    );
+    console.log("userCredential", userCredential);
+    state.value = STATE.USER_FOUND;
+  } catch (error: any) {
+    console.log(error);
+    state.value = STATE.ERROR_REQUEST;
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,58 @@
 <template>
-  <NuxtLink to="/guides">Go to guides pages</NuxtLink>
+  <template v-if="state === STATE.USER_ENTERED">
+    <div class="flex items-center justify-center gap-8 p-4">
+      <p>{{ user?.email }}</p>
+      <UButton variant="ghost" @click="logout">Logout</UButton>
+    </div>
+  </template>
+  <template v-if="state === STATE.USER_NOT_ENTERED">
+    <div class="flex h-dvh w-full justify-center align-middle">
+      <PGAuthForm />
+    </div>
+  </template>
+  <template v-if="state === STATE.USER_ENTERED">
+    <div class="flex flex-1 items-center justify-center">
+      <NuxtLink class="block text-center underline" to="/guides"
+        >Go to guides pages
+      </NuxtLink>
+    </div>
+  </template>
 </template>
+
+<script lang="ts" setup>
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { definePageMeta } from "#imports";
+
+definePageMeta({
+  title: "Personal Guide Main Page",
+});
+
+enum STATE {
+  INITIAL = "INITIAL",
+  USER_ENTERED = "USER_ENTERED",
+  USER_NOT_ENTERED = "USER_NOT_ENTERED",
+}
+
+const state = ref(STATE.INITIAL);
+
+const user = useCurrentUser();
+const auth = getAuth();
+
+const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error("Error when exiting", error);
+  }
+};
+
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      state.value = STATE.USER_ENTERED;
+    } else {
+      state.value = STATE.USER_NOT_ENTERED;
+    }
+  });
+});
+</script>
