0053a85b77f8cca11569cf50bab8f303e8e3321e
artem-alekseenko <ivanovo.cwi@gmail.com>
2024-12-22 15:17:05 +0300

    add persist store

--- stores/guidesStore.ts
+++ stores/guidesStore.ts
@@ -3,53 +3,65 @@ import type { IGuide } from "~/types";
 import { ref } from "vue";
 import { useGuides } from "~/composables/useGuides";
 
-export const useGuidesStore = defineStore("guidesStore", () => {
-  // State
-  const _guidesList = ref<IGuide[]>([]);
-  const isGuidesListLoading = ref<boolean>(false);
-  const _selectedGuideId = ref<string | null>(null);
+export const useGuidesStore = defineStore(
+  "guidesStore",
+  () => {
+    // State
+    const _guidesList = ref<IGuide[]>([]);
+    const isGuidesListLoading = ref<boolean>(false);
+    const _selectedGuideId = ref<string | null>(null);
 
-  // Getters
-  const selectedGuide = computed((): IGuide | null => {
-    if (!_selectedGuideId.value) {
-      return null;
-    }
+    // Getters
+    const selectedGuide = computed((): IGuide | null => {
+      if (!_selectedGuideId.value) {
+        return null;
+      }
 
-    return (
-      _guidesList.value.find((guide) => guide.id === _selectedGuideId.value) ||
-      null
-    );
-  });
-  const guidesList = computed((): IGuide[] => _guidesList.value);
+      return (
+        _guidesList.value.find(
+          (guide) => guide.id === _selectedGuideId.value,
+        ) || null
+      );
+    });
+    const guidesList = computed((): IGuide[] => _guidesList.value);
 
-  // Actions
-  const setSelectedGuideId = (newGuide: string): void => {
-    _selectedGuideId.value = newGuide;
-  };
-  const setGuidesList = (newGuides: IGuide[]) => {
-    _guidesList.value = newGuides;
-    isGuidesListLoading.value = false;
-  };
-  const fetchGuidesList = async () => {
-    if (isGuidesListLoading.value || guidesList.value.length) {
-      return;
-    }
-    isGuidesListLoading.value = true;
-    try {
-      const requestedGuides = await useGuides();
-      setGuidesList(requestedGuides);
-    } catch (error) {
+    // Actions
+    const setSelectedGuideId = (newGuide: string): void => {
+      _selectedGuideId.value = newGuide;
+    };
+    const setGuidesList = (newGuides: IGuide[]) => {
+      _guidesList.value = newGuides;
       isGuidesListLoading.value = false;
-      throw error;
-    }
-  };
+    };
+    const fetchGuidesList = async () => {
+      if (isGuidesListLoading.value || guidesList.value.length) {
+        return;
+      }
+      isGuidesListLoading.value = true;
+      try {
+        const requestedGuides = await useGuides();
+        setGuidesList(requestedGuides);
+      } catch (error) {
+        isGuidesListLoading.value = false;
+        throw error;
+      }
+    };
 
-  return {
-    guidesList,
-    isGuidesListLoading,
-    setGuidesList,
-    fetchGuidesList,
-    selectedGuide,
-    setSelectedGuideId,
-  };
-});
+    return {
+      guidesList,
+      isGuidesListLoading,
+      setGuidesList,
+      fetchGuidesList,
+      selectedGuide,
+      setSelectedGuideId,
+      _selectedGuideId,
+      _guidesList,
+    };
+  },
+  {
+    persist: {
+      storage: piniaPluginPersistedstate.localStorage(),
+      pick: ["_selectedGuideId", "_guidesList"],
+    },
+  },
+);
