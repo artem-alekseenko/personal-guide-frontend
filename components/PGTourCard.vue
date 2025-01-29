5864b090a7651989a46d6978615ad89d22da80e0
artem-alekseenko <ivanovo.cwi@gmail.com>
2025-01-29 21:39:20 +0300

    PG-5: change tours list design and add long polling for fetchListTours

---
+++ components/PGTourCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    :style="{ backgroundImage: `url(${imageUrl})` }"
+    class="lg:grid-row-3 relative grid grid-cols-1 items-center justify-start justify-items-center gap-x-16 gap-y-4 rounded-xl border border-solid border-lime-200 bg-cover bg-center p-4 shadow-xl hover:border-gray-200 hover:bg-gray-100 md:p-8 lg:w-full lg:grid-cols-4"
+  >
+    <div class="absolute inset-0 rounded-xl bg-black bg-opacity-50"></div>
+    <div class="relative z-10">
+      <h2 class="prose text-xl font-bold text-white">{{ name }}</h2>
+      <p class="prose text-lg font-semibold text-slate-100">
+        {{ description }}
+      </p>
+      <p class="text-slate-100">Guide: {{ guideName }}</p>
+      <p v-if="generatingPercent !== 100" class="prose">
+        <UProgress :value="generatingPercent" indicator />
+      </p>
+      <p class="prose text-green-200">{{ generatingText }}</p>
+      <NuxtLink
+        v-if="generatingPercent === 100"
+        :to="`/tours/${tourId}`"
+        class="prose block text-right text-blue-100 underline"
+      >
+        Go to the tour
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  name: string;
+  description: string;
+  generatingPercent: number;
+  generatingText: string;
+  tourId: string;
+  imageUrl: string;
+  guideName: string;
+}>();
+</script>
--- pages/tours/index.vue
+++ pages/tours/index.vue
@@ -5,24 +5,17 @@
       Create new tour
     </PGButton>
     <div class="flex flex-col justify-center gap-4 p-4 md:p-12">
-      <div
+      <PGTourCard
         v-for="tour in routeStore.allTours"
-        class="lg:grid-row-3 grid grid-cols-1 items-center justify-start justify-items-center gap-x-16 gap-y-4 rounded-xl border border-solid border-lime-200 p-4 shadow-xl hover:border-gray-200 hover:bg-gray-100 md:p-8 lg:w-full lg:grid-cols-4"
-      >
-        <h2 class="prose">{{ tour.name }}</h2>
-        <p class="prose">{{ tour.description }}</p>
-        <p v-if="tour.generating_percent !== 100" class="prose">
-          Generated percent: {{ tour.generating_percent }}
-        </p>
-        <p v-else class="prose text-green-800">The tour has been generated!</p>
-        <NuxtLink
-          v-if="tour.generating_percent === 100"
-          :to="`/tours/${tour.id}`"
-          class="prose text-blue-800 underline"
-        >
-          Go to the tour
-        </NuxtLink>
-      </div>
+        :key="tour.id"
+        :description="tour.description"
+        :generatingPercent="tour.generating_percent"
+        :generatingText="tour.generating_string"
+        :guideName="tour.guide.name"
+        :imageUrl="tour.image"
+        :name="tour.name"
+        :tourId="tour.id"
+      />
     </div>
   </section>
 </template>
